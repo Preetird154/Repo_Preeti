1acbd2152a2c83f72a114a47ee42f5849db0b17c
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-12 10:38:52 +0530

    written program using throws keyword

---
+++ Git/src/GitHub/Throws_KeyWord.java
@@ -0,0 +1,33 @@
+package GitHub;
+
+public class Throws_KeyWord {
+	public void alpha() throws Exception
+	{
+		System.out.println("alpha is starting");
+		int a=10;
+		int b=0;
+		int c=a/b;
+		System.out.println("alpha is ending");
+	}
+	
+	public void beta() throws Exception
+	{
+		System.out.println("beta is starting");
+		alpha();
+		System.out.println("beta is ending");
+	}
+	
+	public static void main(String[] args) {
+		
+		try
+		{
+			new Throws_KeyWord().beta();
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("Exception was handled by main()");
+		}
+		
+	}
+}
