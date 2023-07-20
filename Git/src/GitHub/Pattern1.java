70733d37b1b67cb23d569ea87d99aefb00a47718
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-20 10:33:52 +0530

    Pattern program1

--- Git/src/GitHub/Pattern1.java
+++ Git/src/GitHub/Pattern1.java
@@ -2,33 +2,23 @@ package GitHub;
 
 public class Pattern1 {
 	public static void main(String[] args) {
-		int n=5,res=0;
-		for(int i=0;i<n;i++)
+		int n=3;
+		for(int r=1;r<=n;r++)
 		{
-			for(int s=0;s<n-i;s++)
+			for(int s=r;s<n;s++)
 			{
 				System.out.print(" ");
 			}
-			for(int c=0;c<=i;c++)
+			
+			for(int c=1;c<=r;c++)
 			{
-				int ifact=1;
-				for(int k=1;k<=i;k++)
-				{
-					ifact=ifact*k;
-				}
-			    int ijfact=1;
-			    for(int ij=1;ij<=i-c;ij++)
-			    {
-			    	ijfact=ijfact*ij;
-			    }
-			    int jfact=1;
-			    for(int m=1;m<=c;m++)
-			    {
-			    	jfact=jfact*m;
-			    }
-			    res=(ifact)/(ijfact*jfact);
-			   System.out.print(res+" ");
+				System.out.print("*");
 			}
+			for(int c=1;c<r;c++)
+			{
+				System.out.print("*");
+			}
+			
 			System.out.println();
 		}
 	}
