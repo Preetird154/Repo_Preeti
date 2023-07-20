5decf8e798d111901614269e3725e08ee423f1dc
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-20 11:13:51 +0530

    Pattern Program7

---
+++ Git/src/GitHub/Pattern7.java
@@ -0,0 +1,22 @@
+package GitHub;
+
+public class Pattern7 {
+	public static void main(String[] args) {
+		int n=5;
+		for(int r=n;r>=1;r--)
+		{
+			for(int s=1;s<=n-r;s++)
+			{
+				System.out.print(" ");
+			}
+			
+			
+			for(int c=1;c<=r;c++)
+			{
+				System.out.print(r);
+			}
+			System.out.println();
+		}
+	}
+
+}
