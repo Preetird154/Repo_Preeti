9b450c32e4749042af3044d941f5b7df8821e61c
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-21 11:10:38 +0530

    Pattern Program 19

---
+++ Git/src/GitHub/Pattern19.java
@@ -0,0 +1,21 @@
+package GitHub;
+
+public class Pattern19 {
+	public static void main(String[] args) {
+		int n=5;
+		for(int r=1;r<=n;r++)
+		{
+			for(int s=1;s<=n-r;s++)
+			{
+				System.out.print(" ");
+			}
+			for(int c=1;c<=r;c++)
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+
+}
