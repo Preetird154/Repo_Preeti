5fc0ad41b3ab551d28a26c7f68c741e8f81953c5
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-26 10:26:38 +0530

    program to find factorial of given number

---
+++ Git/src/GitHub/Factorial.java
@@ -0,0 +1,19 @@
+package GitHub;
+
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please enter the number");
+		int n=sc.nextInt();
+		int res=1;
+		for(int i=1;i<=n;i++)
+		{
+			res=res*i;
+		}
+		System.out.println(res);
+	}
+
+
+}
