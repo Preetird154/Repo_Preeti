3c380ebe23cdb6b3416541af2c426925acfa8fa7
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-28 23:02:10 +0530

    first navigation

--- selenium5/src/selenium5/Demo.java
+++ selenium5/src/selenium5/Demo.java
@@ -1,13 +1,36 @@
 package selenium5;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver d=new ChromeDriver();
-		d.get("https://www.google.com");
+		d.get("https://opensource-demo.orangehrmlive.com");
+		
+		d.manage().window().maximize();
+		Thread.sleep(3000);
+		
+		By b=By.xpath("//input[@name=\"username\"]");
+		WebElement ele=d.findElement(b);
+		ele.sendKeys("Admin");
+		
+		Thread.sleep(3000);
+		
+		By b1=By.xpath("//input[@name=\"password\"]");
+		WebElement ele1=d.findElement(b1);
+		ele1.sendKeys("admin123");
+		
+		Thread.sleep(3000);
+		
+
+		By b2=By.xpath("//button[ text()=\" Login \"]");
+		WebElement ele2=d.findElement(b2);
+		ele2.click();
+		
 
 	}
 
