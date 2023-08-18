80f1869036d3870e171a5941cf7ed8128d77aec2
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-08-18 18:14:10 +0530

    commit

--- Window_Handling/src/window_handling/hyr_tutorials.java
+++ Window_Handling/src/window_handling/hyr_tutorials.java
@@ -5,32 +5,28 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class hyr_tutorials extends Base_class 
-{
+public class hyr_tutorials extends Base_class {
 
-	static By button = By.id("newWindowBtn");
+	//static By button = By.id("newWindowBtn");
 
-	public static WebElement getbutton()
-{
-		String parentHandle = driver.getWindowHandle();
-		System.out.println("Parent Window - " + parentHandle);
-		WebElement btn = driver.findElement(button);
-		return btn;
-	}
-
-	public static void button() {
-		getbutton().click();
-		driver.close();
-	}
-
-	
-		Set<String> Handles = driver.getWindowHandles();
-
-		for (String handle : Handles) 
-		{
-			System.out.println(handle);
-		}
-
-	
-
+//	public static WebElement getbutton() {
+//		String parentHandle = driver.getWindowHandle();
+//		System.out.println("Parent Window - " + parentHandle);
+//		WebElement btn = driver.findElement(button);
+//		return btn;
+//	}
+//
+//	public static void button() 
+//	{
+//		getbutton().click();
+//		driver.close();
+//	}
+//
+//	// Set<String> Handles = driver.getWindowHandles();
+//
+//	for(String handle:Handles)
+//	{
+//		System.out.println(handle);
+//	}
+//
 }
