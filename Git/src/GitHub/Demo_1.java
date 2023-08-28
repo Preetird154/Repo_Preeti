e74b78f523406ebc5168ac11a2f9d8fef1dd62e6
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-08-28 18:01:37 +0530

    commit

---
+++ Git/src/GitHub/Demo_1.java
@@ -0,0 +1,12 @@
+package GitHub;
+
+public class Demo_1 {
+	public static void main(String[] args) {
+		
+		String s1="hi Hello";
+		String s2="hi hello";
+		
+		System.out.println(s1.equals(s2));
+	}
+
+}
